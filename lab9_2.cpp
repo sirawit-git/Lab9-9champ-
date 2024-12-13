60c271c9102fa670926dac87161912f1e1a1628d
Champ <champsrw49@gmail.com>
2024-12-14 00:25:47 +0700

    Lab9_2 complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int N,int M){
+    if (M <= 0 || N <= 0){
+        cout << "Invalid input";  
+    }else{
+        for(int i = 0;i < N;i++){
+            for(int x = 0; x < M;x++){
+                cout << "O";
+            }
+        
+            cout << endl;
+        }
+    }
+}
+
 int main(){
 	
 	printO(2,2);
